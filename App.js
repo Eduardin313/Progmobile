584b83a8c5c80fa773773b829cb44c9473bcb1fc
Eduardo <e.oliveira.moraes16@gmail.com>
2025-11-17 10:46:35 -0300

    commit2

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import TelaInicial from './src/component/TelaInicial';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TelaInicial></TelaInicial>
     </View>
   );
 }
---
+++ src/component/TelaInicial.js
@@ -0,0 +1,11 @@
+import { View, Image, TextInput, TouchableOpacity, Text } from 'react-native';
+
+const TelaInicial = () => {
+
+    return (
+        <View>
+            <Text>Tela Funcionando!</Text>
+        </View>
+    );
+}
+export default TelaInicial;
